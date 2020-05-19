ce46dbf3cffbd3483f7ddff3abdad1e6b88ec79d
Agang <agangdut@gmail.com>
2020-05-18 20:28:13 -0400

    1
    1

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -1,25 +1,33 @@
 $(document).ready( function () {
-    // I only have one form on the page but you can be more specific if need be.
-    var $form = $('form');
+    var $form = $('email-form');
 
-    if ( $form.length > 0 ) {
-        $('form input[type="submit"]').bind('click', function ( event ) {
-            if ( event ) event.preventDefault();
-            // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
-            if ( validate_input($form) ) { register($form); }
+    if ($form.length > 0) {
+        $('form input[type="submit"]').bind('click', function (event) {
+            if (event) event.preventDefault();
+            if ( validate_input($form) ) {
+                 emailSubscribe($form); 
+            }
         });
     }
 });
 
-function register($form) {
+function validate_input($form) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) {
+        return true 
+    } else {
+        alert("Please enter a valid email!")
+    }
+}
+
+function emailSubscribe($form) {
     $.ajax({
         type: $form.attr('method'),
         url: $form.attr('action'),
         data: $form.serialize(),
         cache       : false,
         dataType    : 'json',
-        contentType: "application/json; charset=utf-8",
-        error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+        contentType : "application/json; charset=utf-8",
+        error       : function(err) { alert("An error occured with the server. Please try again later."); },
         success     : function(data) {
             if (data.result != "success") {
                 alert("An error has occured, please try again later.")
